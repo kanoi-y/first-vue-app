b7024fddb7a0be182176f30bbf9ca05e9bcf63b4
kanoi-y <furafuratonbo@gmail.com>
2022-10-18 22:37:36 +0900

    🎉 init commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+const isActive = true;
+</script>
+
+<template>
+  <h1>Vue 3 入門</h1>
+  <p class="underLine" :class="{ active: isActive }">v-bindの設定方法の確認</p>
+</template>
+
+<style>
+.active {
+  color: red;
+  font-weight: 900;
+}
+.underLine {
+  text-decoration: underline;
+}
+</style>
