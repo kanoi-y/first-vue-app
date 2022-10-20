ab4f2033711d53b58229989d76a27869d005c125
kanoi-y <furafuratonbo@gmail.com>
2022-10-20 14:50:44 +0900

    ✨ Input.vueを実装

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <script setup>
-import Hello from './components/Hello.vue';
+import { ref } from "vue";
+import Input from "./components/Input.vue";
+import Hello from "./components/Hello.vue";
+
+const name = ref("John Doe");
+const address = ref("");
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <Hello message="Propsの使い方1" />
+  <p>name:{{ name }}</p>
+  <p>address:{{ address }}</p>
+  <Input v-model="name" />
+  <Input v-model="address" />
+  <Hello :name="name" />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,10 +1,25 @@
 <script setup>
+import { ref } from 'vue';
 const props = defineProps({
-    message:String
-})
+  name: String,
+});
+
+const name = ref(props.name);
+
+const emit = defineEmits({
+  changeNameEvent: (name) => {
+    if (name) return true;
+    return false;
+  },
+});
+
+const changeName = () => {
+  emit('changeNameEvent', name.value);
+};
 </script>
 
 <template>
-  <h2>初めてのコンポーネント</h2>
-  <p>{{ props.message }}</p>
+  <h2>子コンポーネント</h2>
+  <p>Hello {{ props.name }}</p>
+  <input type="text" v-model="name" @input="changeName" />
 </template>
